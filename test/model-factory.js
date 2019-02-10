6bf73657d621d9312f27482a76fc61584e2e4649
DominicSherman <dominic.sherman98@gmail.com>
2019-02-09 19:35:26 -0600

    Add model factory

---
+++ test/model-factory.js
@@ -0,0 +1,63 @@
+import Chance from 'chance';
+
+const chance = new Chance();
+
+export const createRandomRound = (round = {}) => ({
+    playerFour: {
+        actual: chance.natural(),
+        bid: chance.natural()
+    },
+    playerOne: {
+        actual: chance.natural(),
+        bid: chance.natural()
+    },
+    playerThree: {
+        actual: chance.natural(),
+        bid: chance.natural()
+    },
+    playerTwo: {
+        actual: chance.natural(),
+        bid: chance.natural()
+    },
+    score: chance.natural(),
+    ...round
+});
+
+export const createRandomProps = (props = {}) => ({
+    actions: {
+        restart: jest.fn(),
+        toggleShowHistory: jest.fn(),
+        toggleShowInfoModal: jest.fn()
+    },
+    bids: {
+        playerFour: {
+            bid: chance.natural()
+        },
+        playerOne: {
+            bid: chance.natural()
+        },
+        playerThree: {
+            bid: chance.natural()
+        },
+        playerTwo: {
+            bid: chance.natural()
+        }
+    },
+    currRound: {
+        player1Bid: chance.natural(),
+        player2Bid: chance.natural(),
+        player3Bid: chance.natural(),
+        player4Bid: chance.natural()
+    },
+    isBids: chance.natural(),
+    rounds: chance.n(createRandomRound, chance.d6() + 1),
+    team1: {
+        firstPlayer: chance.string(),
+        secondPlayer: chance.string()
+    },
+    team2: {
+        fourthPlayer: chance.string(),
+        thirdPlayer: chance.string()
+    },
+    ...props
+});
